c3c5cbbe76d43046f69d253306a23d9f819e1c2a
JGWill Gaia.WSL <jgi@jgwill.com>
2023-11-26 11:35:18 -0500

    gal

--- src/app.js
+++ src/app.js
@@ -9,9 +9,23 @@ const imgList = [
 // Get the image container
 const imageContainer = document.getElementById('image-container');
 
+// Get the modal
+const modal = document.getElementById('modal');
+const modalContent = document.getElementById('modal-content');
+const close = document.getElementById('close');
+
 // For each image path, create an img element and append it to the container
 imgList.forEach((imgPath) => {
   const img = document.createElement('img');
   img.src = imgPath;
+  img.onclick = function() {
+    modal.style.display = "block";
+    modalContent.src = this.src;
+  }
   imageContainer.appendChild(img);
 });
+
+// When the user clicks on <span> (x), close the modal
+close.onclick = function() {
+  modal.style.display = "none";
+}
